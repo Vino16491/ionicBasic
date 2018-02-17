34353cf3adc0bfb941eb3d8f69559d43b559034c
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-02-17 15:16:14 +0530

    TemperatureConvert Function created

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,30 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  temp = {
+    celsius: null,
+    fahrenheit: null
+  }
   constructor(public navCtrl: NavController) {
 
+  }
+  tempConvertFahrenheit() {
+    // this.temp.fahrenheit = this.temp.celsius;
+    if(this.temp.celsius != null){
+      this.temp.fahrenheit = ((this.temp.celsius) * 9/5) + 32 + ' °F'
+    }
+    if(this.temp.celsius === ''){
+      this.temp.fahrenheit = ''
+    }
+  }
+  tempConvertCelsius(){
+    if(this.temp.fahrenheit != null){
+      this.temp.celsius = ((this.temp.fahrenheit) - 32) * 5/9 + ' °C'
+    }
+
+    if(this.temp.fahrenheit === ''){
+      this.temp.celsius = ''
+    }
   }
 
 }
